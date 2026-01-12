a23b54e6671a23655edb5de9ce415b51f6baab80
Jason Mustafa <mustafa.jason@gmail.com>
2026-01-11 23:59:58 -0800

    refactor(chezmoi): move source directory to home/

--- home/run_onchange_configure-fish.fish
+++ home/run_onchange_configure-fish.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+fisher update
+
 fish_config theme choose "Catppuccin Mocha"
 
 tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='12-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
