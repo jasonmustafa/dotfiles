ac9ebedb3d0302243ffc8c0dab69ac259343e289
Jason Mustafa <mustafa.jason@gmail.com>
2026-01-11 22:30:50 -0800

    feat(fish): add fish config files

---
+++ dot_config/private_fish/conf.d/myabbrs.fish
@@ -0,0 +1,20 @@
+abbr -a -- ch cht.sh
+abbr -a -- j just
+
+abbr -a -- n nvim
+
+abbr -a -- l eza --group-directories-first --icons=auto
+abbr -a -- la eza -a --git --group-directories-first --icons=auto
+abbr -a -- ll eza -l --git --group-directories-first --icons=auto
+abbr -a -- lla eza -la --git --group-directories-first --icons=auto
+
+abbr -a -- rm rm -i
+
+# Git
+abbr -a -- ga git add
+abbr -a -- gb git branch
+abbr -a -- gc git commit
+abbr -a -- gf git fetch
+abbr -a -- gl git log
+abbr -a -- gs git status
+abbr -a -- gsw git switch
---
+++ run_onchange_configure-tide.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+fish_config theme choose "Catppuccin Mocha"
+
+tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time='12-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
