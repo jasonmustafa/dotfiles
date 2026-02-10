0c9257eaa818650b1bd188004a39d048783c67da
Jason Mustafa <mustafa.jason@gmail.com>
2026-02-10 02:54:33 -0800

    feat(nvim): base config (insanely based)
    https://youtu.be/xGkL2N8w0H4

---
+++ home/dot_config/nvim/init.lua
@@ -0,0 +1,44 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+
+vim.lsp.enable({ "lua_ls", "ty" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.cmd("colorscheme catppuccin-mocha")
