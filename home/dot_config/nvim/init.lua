4e759e2299a00a32d7ada6fcf1d866ffcff742c3
Jason Mustafa <mustafa.jason@gmail.com>
2026-02-12 00:49:15 -0800

    feat: more plugins

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -25,9 +25,51 @@ vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/aznhe21/actions-preview.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.icons" }, -- for mini.pick
+	{ src = "https://github.com/nvim-mini/mini.pick" }, -- requires mini.icons, ripgrep
+	{ src = "https://github.com/nvim-mini/mini.pairs" },
+	{ src = "https://github.com/nvim-mini/mini.surround" },
+	{ src = "https://github.com/nvim-mini/mini.statusline" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/vyfor/cord.nvim" },
+	{ src = "https://github.com/saghen/blink.cmp" },
+	{ src = "https://github.com/akinsho/toggleterm.nvim" },
+	{ src = "https://github.com/folke/trouble.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" }
 })
+
+require 'mini.icons'.setup()
+
+require "trouble".setup()
+
+require "toggleterm".setup {
+	open_mapping = [[<c-\>]],
+	direction = "float",
+}
+
+-- Remove unused plugins
+local function pack_clean()
+	local active_plugins = {}
+	local unused_plugins = {}
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		active_plugins[plugin.spec.name] = plugin.active
+	end
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not active_plugins[plugin.spec.name] then
+			table.insert(unused_plugins, plugin.spec.name)
+		end
+	end
+
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+	if choice == 1 then
+		vim.pack.del(unused_plugins)
+	end
+end
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
@@ -39,10 +81,47 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.cmd("set completeopt+=noselect")
 
+require "blink.cmp".setup {
+	-- To install rust fuzzy finder
+	-- cd $HOME/.local/share/nvim/site/pack/core/opt/blink.cmp
+	-- cargo build --release
+	fuzzy = { implementation = "rust" }
+}
+
 require "mini.pick".setup()
-require "oil".setup()
+require "mini.pairs".setup()
+require "mini.surround".setup()
+require "mini.statusline".setup()
+
+require "oil".setup {
+	columns = { "permissions", "icon" },
+	view_options = { show_hidden = true },
+}
+
+require "gitsigns".setup {
+	signs = {
+		add = { text = '+' },
+		change = { text = '~' },
+		delete = { text = '_' },
+		topdelete = { text = '-' },
+		changedelete = { text = '~' },
+	}
+}
+
+require "mason".setup()
+require "catppuccin".setup { transparent_background = true }
+
+require "cord".setup {
+	text = {
+		editing = 'Editing file',
+		workspace = '',
+	}
+}
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>g', ":Pick grep_live<CR>")
+vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>")
+
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 
@@ -59,5 +138,7 @@ vim.lsp.config("lua_ls", {
 })
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>pc', pack_clean)
+vim.keymap.set('n', '<leader>q', ":Trouble diagnostics toggle<CR>")
 
 vim.cmd("colorscheme catppuccin-mocha")
